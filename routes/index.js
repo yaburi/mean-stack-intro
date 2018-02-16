f0731c898504c91afdc9dabf59910d57cb5709a6
yaburi <nathan.au@hotmail.com>
2018-02-16 16:27:06 +1100

    pulled data from database

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express test' });
+});
+
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hellow, World!' });
+});
+
+// Get userlist page
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({}, {}, function(e, docs) {
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
 });
 
 module.exports = router;
