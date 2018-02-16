4f2d90f3db4e4f2fa876d3cb9c917f6abc4a6ada
yaburi <nathan.au@hotmail.com>
2018-02-16 19:30:03 +1100

    finished basics

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,37 @@ router.get('/userlist', function(req, res) {
     });
 });
 
+// Get newUser page
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title : 'Add New User' });
+});
+
+// POST to addUser service
+router.post('/adduser', function(req, res) {
+
+    // set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+    }, function(err, doc) {
+        if (err) {
+            // if it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            // else forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
